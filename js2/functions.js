da63bd020830f31ceac11eec955142ebfb20a0be
Matheus Chaves <111753767+chaves3@users.noreply.github.com>
2024-02-13 19:25:20 -0300

    Add files via upload

---
+++ js2/functions.js
@@ -0,0 +1,108 @@
+
+//função para abrir modal página de projetos
+    abrirJanela();
+    function abrirJanela(){
+    
+      $('.btn').click(function(e){
+          e.stopPropagation();
+          $('.bg').show();
+      });
+    }
+//função para abrir modal2 página de projetos
+    abrirJanela2();
+    function abrirJanela2(){
+    
+      $('.btn5').click(function(e){
+          e.stopPropagation();
+          $('.bg5').show();
+      });
+    }
+
+//função para fechar modal página de projetos
+    verificarCliqueFechar();
+    function verificarCliqueFechar(){
+      var el = $('.closeBtn');
+      el.click(function(){
+        $('.bg').fadeOut();
+        $('.bg2').fadeOut();
+        $('.bg3').fadeOut();
+        $('.bg4').fadeOut();
+      });
+      $('.form').click(function(e){
+        e.stopPropagation();
+      });
+    }
+
+
+//função para fechar modal2 página de projetos
+
+    verificarCliqueFechar2();
+    function verificarCliqueFechar2(){
+      var el = $('.closeBtn2');
+      el.click(function(){
+        $('.bg5').fadeOut();
+        $('.bg6').fadeOut();
+        $('.bg7').fadeOut();
+        $('.bg8').fadeOut();
+
+      });
+      $('.form2').click(function(e){
+        e.stopPropagation();
+      });
+    }
+
+   
+      
+
+//comferindo cada conteúdo do modal
+      
+      $( "a" ).click(function(event) {
+        if ($( this ).is( ".btn2" ) ) {
+            $('.bg2').show();
+        }
+        event.preventDefault();
+      });
+
+      $( "a" ).click(function(event) {
+        if ($( this ).is( ".btn3" ) ) {
+            $('.bg3').show();
+        }
+        event.preventDefault();
+      });
+
+      $( "a" ).click(function(event) {
+        if ($( this ).is( ".btn4" ) ) {
+            $('.bg4').show();
+        }
+        event.preventDefault();
+      });
+
+      $( "a" ).click(function(event) {
+        if ($( this ).is( ".btn5" ) ) {
+            $('.bg5').show();
+        }
+        event.preventDefault();
+      });
+
+      $( "a" ).click(function(event) {
+        if ($( this ).is( ".btn6" ) ) {
+            $('.bg6').show();
+        }
+        event.preventDefault();
+      });
+
+      $( "a" ).click(function(event) {
+        if ($( this ).is( ".btn7" ) ) {
+            $('.bg7').show();
+        }
+        event.preventDefault();
+      });
+
+      $( "a" ).click(function(event) {
+        if ($( this ).is( ".btn8" ) ) {
+            $('.bg8').show();
+        }
+        event.preventDefault();
+      });
+
+    
